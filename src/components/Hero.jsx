f3fd46badb2d64ecc476b77a15520a42697bfa9d
Michael <88445485+mjnyland@users.noreply.github.com>
2023-08-28 13:15:59 -0400

    Updated calendly event link

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -115,7 +115,7 @@ const Hero = () => {
               animate="visible"
             >
               <PopupButton
-                url="https://calendly.com/michael-csc/daysheets-demo"
+                url="https://calendly.com/benatdaysheets/30"
                 /*
                  * react-calendly uses React's Portal feature (https://reactjs.org/docs/portals.html) to render the popup modal. As a result, you'll need to
                  * specify the rootElement property to ensure that the modal is inserted into the correct domNode.
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -138,7 +138,7 @@ const Navbar = () => {
               className={`App bg-blue text-black text-center ${styles.buttonBlack}`}
             >
               <PopupButton
-                url="https://calendly.com/michael-csc/daysheets-demo"
+                url="https://calendly.com/benatdaysheets/30"
                 /*
                  * react-calendly uses React's Portal feature (https://reactjs.org/docs/portals.html) to render the popup modal. As a result, you'll need to
                  * specify the rootElement property to ensure that the modal is inserted into the correct domNode.
