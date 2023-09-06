b08b7e9eaacbb92d83b6efa6dd2988d4035edbc8
Michael <88445485+mjnyland@users.noreply.github.com>
2023-09-06 09:50:19 -0400

    Shortened testimonial

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -48,7 +48,7 @@ const Reviews = () => {
     },
     {
       id: 3,
-      review: `Many of us nerdy managers are constantly seeking better solutions for more efficient tours. I’ve been working with Daysheets on a few tours already, and so far, it is the best tool i’ve seen out there in the market. It’s exactly what I was looking for, something that works for 2023, smart, intuitive and evolving quickly.`,
+      review: `Many of us nerdy managers are constantly seeking better solutions for more efficient tours. So far, it is the best tool i’ve seen out there in the market. It’s exactly what I was looking for, something that works for 2023, smart, intuitive and evolving quickly.`,
       image: avatar,
       name: "Ben Matusow",
       title: "Artist Manager",
